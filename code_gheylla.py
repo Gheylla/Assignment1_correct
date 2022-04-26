15c2c3dda64f315d08c4e2aef0d4f5fc2b6eb6c1
Gheylla <gheylla.liberia@live.com>
2022-04-26 21:17:34 +0200

    Update code_gheylla.py

--- code_gheylla.py
+++ code_gheylla.py
@@ -12,18 +12,29 @@ import scipy as sp
 
 import pandas as pd
 
-wieringermeer_meteo = pd.read_excel(r'C:\Users\User\Desktop\TU DELFT\Master 2021-2022\Q4\Modelling for Coupled Processes\Assignment1_correct\WieringermeerData_LeachateProduction.xlsx')
-wieringermeer_leachate = pd.read_excel(r'C:\Users\User\Desktop\TU DELFT\Master 2021-2022\Q4\Modelling for Coupled Processes\Assignment1_correct\WieringermeerData_Meteo (1).xlsx')
-
-#display(wieringermeer_meteo)
+wieringermeer_leachate = pd.read_excel(r'C:\Users\User\Desktop\TU DELFT\Master 2021-2022\Q4\Modelling for Coupled Processes\Assignment1_correct\WieringermeerData_LeachateProduction.xlsx')
+wieringermeer_meteo = pd.read_excel(r'C:\Users\User\Desktop\TU DELFT\Master 2021-2022\Q4\Modelling for Coupled Processes\Assignment1_correct\WieringermeerData_Meteo (1).xlsx')
 
 
-
-plt.plot(wieringermeer_leachate)
-plt.xlabel("days")
+plt.plot(wieringermeer_leachate.Leachate)
 plt.ylabel("Leachate production in m3/day")
 plt.title("Leachate Production")
 plt.grid()
+
+beta = 1
+J = 1
+
+L_cl =[10,10]
+L_wd = [10, 10]
+E = 10
+
+#differential functions 
+
+def dYdt(t, Y):
+    """ Return the growth rate of fox and rabbit populations. """
+    return np.array([ J - L_cl[0] - E ,
+                     (1 - beta) * L_cl[0] - L_wd[1]])
+
 
 
 
