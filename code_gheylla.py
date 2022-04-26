b0b266f44288b6341cfec464b77e985aa303cc1e
Gheylla <gheylla.liberia@live.com>
2022-04-26 21:20:35 +0200

    Update code_gheylla.py

--- code_gheylla.py
+++ code_gheylla.py
@@ -8,7 +8,7 @@ Created on Tue Apr 26 20:37:08 2022
 import numpy as np 
 import matplotlib.pyplot as plt 
 import scipy as sp
-#import MyTicToc as mt
+import MyTicToc as mt
 
 import pandas as pd
 
@@ -36,9 +36,56 @@ def dYdt(t, Y):
                      (1 - beta) * L_cl[0] - L_wd[1]])
 
 
+def main():
+    # Definition of output times
+    tOut = np.linspace(0, 100, 200)              # time
+    nOut = np.shape(tOut)[0]
 
+    # Initial case, 10 rabbits, 5 foxes
+    Y0 = np.array([10, 5])
+    mt.tic()
+    t_span = [tOut[0], tOut[-1]]
+    YODE = sp.integrate.solve_ivp(dYdt, t_span, Y0, t_eval=tOut, 
+                                  method='RK45', vectorized=True, 
+                                  rtol=1e-5 )
+    # infodict['message']                     # >>> 'Integration successful.'
+    rODE = YODE.y[0,:]
+    fODE = YODE.y[1,:]
 
+    '''EULER'''
+    # Initialize output vector
+    YEuler = np.zeros([nOut, 2], dtype=float)
 
+    dtMax = 0.1
+    # dtMin = 1e-11
+    t = tOut[0]
+    iiOut = 0
+
+    # Initialize problem
+    mt.tic()
+    Y = Y0
+    # Write initial values to output vector
+    YEuler[iiOut, [0, 1]] = Y
+    while (t < tOut[nOut-1]):
+        # check time steps
+        Rates = dYdt(t, Y)
+        dtRate = -0.7 * Y/(Rates + 1e-18)
+        dtOut = tOut[iiOut+1]-t
+        dtRate = (dtRate <= 0)*dtMax + (dtRate > 0)*dtRate
+        dt = min(min(dtRate), dtOut, dtMax)
+
+        Y = Y + Rates * dt
+        t = t + dt
+
+        # print ("Time to Output is " + str(np.abs(tOut[iiOut+1]-t)) +
+        # " days.")
+
+        if (np.abs(tOut[iiOut+1]-t) < 1e-5):
+            YEuler[iiOut+1, [0, 1]] = Y
+            iiOut += 1
+
+    rEuler, fEuler = YEuler.T
+    mt.toc()
 
 
 
