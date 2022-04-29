d22581563a4be20bb1cd0bcb2908942c5df1a3bb
Gheylla <gheylla.liberia@live.com>
2022-04-29 11:48:25 +0200

    Update code_gheylla.py

--- code_gheylla.py
+++ code_gheylla.py
@@ -53,6 +53,9 @@ waste = 281083000               #wet weight [kg]
 f_crop = 1                          #Crop factor
 Bo = 1
 L_wd = 2
+L_cl = 1
+beta = 2
+f_red = 1
 
 #differential functions 
 def dYdt(t, Y):
@@ -63,27 +66,8 @@ def dYdt(t, Y):
 #define the other functions 
 #def dSdrdt(t, Y): 
  #   return beta * L_cl + L_wd - Q_dr = 0 
-
-#Leaching rates
-L_cl = a * (((Scl - Scl_min) / (Scl_max - Scl_min)) ** b_cl)
-L_wb = a * (((Swb - Swb_min) / (Swb_max - Swb_min)) ** b_wb)
-
-
-# Evaporation model
-def f_red():
-    if S_cl < S_Evmin:
-        return f_red == 0 
-    elif S_Evmin <= S_cl <= S_Evmax:
-        return f_red == (S_cl - S_Evmin) / (S_Evmax - S_Evmin)
-    else:
-        return f_red == 1
-   
+ 
 E = pEV * f_crop * f_red
-
-
-# beta term that allows a certain fraction of water leaching from the cover layer to directly enter the drainage layer
-beta = beta_0 * ((S_cl - Scl_min) / (Scl_max - Scl_min))
-
 
 def main():
     # Definition of output times
@@ -92,7 +76,7 @@ def main():
 
     # Initial case, 10 rabbits, 5 foxes
     Y0 = np.array([10, 5])
-    mt.tic()
+    #mt.tic()
     t_span = [tOut[0], tOut[-1]]
     YODE = sp.integrate.solve_ivp(dYdt, t_span, Y0, t_eval=tOut, 
                                   method='RK45', vectorized=True, 
@@ -103,50 +87,6 @@ def main():
 
 
 
-
-
-
-
-
-
-
-
-
-
-#    '''EULER'''
-    # Initialize output vector
-    YEuler = np.zeros([nOut, 2], dtype=float)
-
-    dtMax = 0.1
-    # dtMin = 1e-11
-    t = tOut[0]
-    iiOut = 0
-
-    # Initialize problem
-    mt.tic()
-    Y = Y0
-    # Write initial values to output vector
-    YEuler[iiOut, [0, 1]] = Y
-    while (t < tOut[nOut-1]):
-        # check time steps
-        Rates = dYdt(t, Y)
-        dtRate = -0.7 * Y/(Rates + 1e-18)
-        dtOut = tOut[iiOut+1]-t
-        dtRate = (dtRate <= 0)*dtMax + (dtRate > 0)*dtRate
-        dt = min(min(dtRate), dtOut, dtMax)
-
-        Y = Y + Rates * dt
-        t = t + dt
-
-        # print ("Time to Output is " + str(np.abs(tOut[iiOut+1]-t)) +
-        # " days.")
-
-        if (np.abs(tOut[iiOut+1]-t) < 1e-5):
-            YEuler[iiOut+1, [0, 1]] = Y
-            iiOut += 1
-
-    rEuler, fEuler = YEuler.T
-    mt.toc()
 
 
 
