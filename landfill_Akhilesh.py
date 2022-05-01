d0ddabf25a0fa77757bbe572c3095308288a5aff
ASoodan25 <104373112+ASoodan25@users.noreply.github.com>
2022-05-01 12:55:48 +0200

    Update landfill_Akhilesh.py

--- landfill_Akhilesh.py
+++ landfill_Akhilesh.py
@@ -19,77 +19,62 @@ rain = np.asarray(table_final.rain)
 evaporation = np.asarray(table_final.pEV)
 Qdr_observed = np.asarray(table_final.leachate)
 
-
-# Definition of parameters
-acl = 0.007     # 0.005~0.01
-awb = 0.0008    # 0.0005~0.001
-Scl_max = 0.60  # 0.45~0.975
-Swb_max = 7.0   # 4.5~7.8
-Scl_min = 0     
-Swb_min = 0
-beta0 = 0.98    # 0~1
-fcrop = 0.92       # 0~1.4
-fred = 1.0
-bcl = 5         # 0~80
-bwb = 30        # 0~80
-base_area = 28355
-S_Evmin = 0
-S_Evmax = 1
-
-table_final['leacheate'] = table_final['leachate']/base_area
-
-def dydt(t, Y):
-
-    #Leaching rates
-
-    Lcl = acl * (((Y[0] - Scl_min) / (Scl_max - Scl_min)) ^ bcl)
-    Lwb = awb * (((Y[1] - Swb_min) / (Swb_max - Swb_min)) ^ bwb)
-    
-#reduction factor reducing evapotranspiration under dry soil conditions
-
-    if Y[0] < S_Evmin:
-        fred = 0
-    elif S_Evmin <= Y[0] <= S_Evmax:
-        fred = (Y[0] - S_Evmin) / (S_Evmax - S_Evmin)
-    else: 
-        fred = 1
-    
-## Total storage in a layer can never exceed the volume of the pore space##
-    if Y[0] < Scl_min:
-        Y[0] = Scl_min
-    elif Y[0] > Scl_max:
+def variables(x):
+    # Definition of parameters
+    acl = 0.007     # 0.005~0.01
+    awb = 0.0008    # 0.0005~0.001
+    Scl_max = 0.60  # 0.45~0.975
+    Swb_max = 7.0   # 4.5~7.8
+    Scl_min = 0     
+    Swb_min = 0
+    beta0 = 0.98    # 0~1
+    fcrop = 0.92       # 0~1.4
+    fred = 1.0
+    bcl = 5         # 0~80
+    bwb = 30        # 0~80
+    S_Evmin = 0
+    S_Evmax = 1
+    x = [acl, awb, Scl_max, Swb_max, Scl_min, Swb_min, beta0, fcrop, fred, bcl, bwb, S_Evmin, S_Evmax]
+    def dYdt(t, Y):
+        Lcl = ((Y[0] - Scl_min)/(Scl_max - Scl_min))
+        Lwb = (Y[1] - Swb_min)/(Swb_max - Swb_min)
+        
+        #Boundaries for values of storage
+        if Y[0] < Scl_min:                  
+            Y[0] = Scl_min
+        elif Y[0] > Scl_max:
             Y[0] = Scl_max   
-    if Y[1] < Swb_min:
+        if Y[1] < Swb_min:
             Y[1] = Swb_min
-    elif Y[1] > Swb_max:
+        elif Y[1] > Swb_max:
             Y[1] = Swb_max 
             
-    # Evaporation model
-    E = evaporation[int(t)] * fcrop * fred
-    
-# B- beta term that allows a certain fraction of water leaching from the cover layer to directly enter the drainage layer
-    B = beta0 * ((Y[0,:] - Scl_min) / (Scl_max - Scl_min)) 
-        
+        #reduction factor reducing evapotranspiration under dry soil conditions
 
-# Definition of Rate Equations
-    dydt = np.zeros(2)
-    dydt[0, 1] = np.array(rain[int(t)] - Lcl - E, 1 - B * Lcl - Lwb)
-    
-    rODE = Y[0,:]
-    fODE = Y[1,:]
-    global rODE
-    global fODE
-    Q_cal = np.zeros(2757)
-    E = np.zeros(2757)
-    
-    # Calculate the storage rate from the measured data
-    
-    Q_cal = beta0 * ((rODE - Scl_min) / (Scl_max - Scl_min)) * acl * ((rODE - Scl_min) / (Scl_max - Scl_min)) ** bcl + awb * ((fODE - Swb_min) / (Swb_max - Swb_min)) ** bwb
-    Q_cal = Q_cal * 28355   # Multiply the area
-    print(Q_cal)
-    Solution = spint.solve_ivp(dydt, t_span, Y0, t_eval=tOut, method='RK45', vectorized=True, rtol=1e-5 )
-    return dydt
+        if Y[0] < S_Evmin:
+            fred = 0
+        elif S_Evmin <= Y[0] <= S_Evmax:
+            fred = (Y[0] - S_Evmin) / (S_Evmax - S_Evmin)
+        else: 
+            fred = 1
 
+        dydt = np.zeros(2)
+        dydt[0] = (rain[int(t)]) - acl*(Lcl)**bcl - (evaporation[int(t)])*fcrop*fred
+        dydt[1] = (1 - beta0*Lcl) * acl*Lcl**bcl - awb*(Lwb)**bwb 
+        return dydt
+    
+    tOut = np.linspace(0, len(evaporation), len(evaporation))              
+    nOut = np.shape(tOut)[0]
+    Y0 = np.array([Scl_max * 0.4, Swb_max*0.92])
+    t_span = [tOut[0], tOut[-1]]
+    YODE = sp.solve_ivp(dYdt, t_span, Y0, t_eval=tOut, method='RK45', vectorized=True, rtol=1e-5 )
+    Qdr_cal = beta0 *((YODE.y[0,:] - Scl_min)/(Scl_max - Scl_min))*acl*((YODE.y[0,:] - Scl_min)/(Scl_max - Scl_min)) ** bcl + awb*((YODE.y[1,:] - Swb_min)/(Swb_max - Swb_min)) ** bwb
+    
+    SSE = np.sum((Qdr_cal - Qdr_observed)**2)
+    return  YODE, Qdr_cal, SSE
+
+base_area = 28355
+table_final['leacheate'] = table_final['leachate']/base_area
 
 # Initial case
 Y0 = np.array([0.8, 0.8])
@@ -99,7 +84,7 @@ tOut = np.linspace(0, len(evaporation), len(evaporation))              # time
 nOut = np.shape(tOut)[0]
 t_span = [tOut[0], tOut[-1]]
 
-Q_rate = np.zeros(2757)
+Q_rate = np.zeros(len(evaporation))
 Q_rate[0] =  0.
 for i in range (1, nOut-1):
     Q_rate[i] = Qdr_observed[i] - Qdr_observed[i-1]
